922976e4dd8580dbc771611a473190a682b7704d
ddooing <stpaq@naver.com>
2023-12-28 17:10:56 +0900

    메뉴바 대공사 중

--- src/main/webapp/js/mypage.js
+++ src/main/webapp/js/mypage.js
@@ -1,16 +1,83 @@
-/**
- * 
- */
+     /*
+      const modal = document.querySelector('.modal');
+        const btnOpenModal=document.querySelector('.archivediv.btn-open-modal');
+		const btnCloseModal=document.querySelector('.btn-close-modal');
+       
+        btnOpenModal.addEventListener("click", ()=>{
+            modal.style.display="flex";
+            console.log("archivediv.btn-open-modal clicked");
+        });
+        btnCloseModal.addEventListener("click", ()=>{
+            modal.style.display="none";
+        });
+        window.addEventListener('click',  (e) => {
+        e.target === modal ?  close() : false
+        })
+        
+         function close(){
+			  modal.style.display="none";
+			 }
+			 
+		*/	 
+		
+		
+		
+		
+		document.addEventListener("DOMContentLoaded", function() {
+		    const modal = document.querySelector('.modal');
+	        const btnOpenModal=document.querySelector('.archivediv.btn-open-modal');
+			const btnCloseModal=document.querySelector('.btn-close-modal');
+	       
+	        btnOpenModal.addEventListener("click", ()=>{
+	            modal.style.display="flex";
+	            console.log("archivediv.btn-open-modal clicked");
+	        });
+	        btnCloseModal.addEventListener("click", ()=>{
+	            modal.style.display="none";
+	        });
+	        window.addEventListener('click',  (e) => {
+	        e.target === modal ?  close() : false
+	        })
+	        
+	         function close(){
+				  modal.style.display="none";
+			}
+			
+			const ratingStars = [...document.getElementsByClassName("rating__star")];
+			
+			
+    if (ratingStars) {
+        ratingStars.addEventListener("click", () => {
+            console.log("ratingStars clicked");
+            // 모달 보여주는 코드
+        });
+    } else {
+        console.log("Element not found");
+    }
+			
+			
+			
+			function executeRating(stars) {
+			  const starClassActive = "rating__star fas fa-star";
+			  const starClassInactive = "rating__star far fa-star";
+			  const starsLength = stars.length;
+			  let i;
+				
+			  stars.map((star) => {
+			    star.onclick = () => {
+			      i = stars.indexOf(star);
+			console.log("*"+i);
+			      if (star.className===starClassInactive) {
+			        for (i; i >= 0; --i) stars[i].className = starClassActive;
+			      } else {
+			        for (i; i < starsLength; ++i) stars[i].className = starClassInactive;
+			      }
+			    };
+			  });
+			}
+			
+			executeRating(ratingStars);
 
- document.addEventListener('DOMContentLoaded', function () {
-    const mypageLink = document.getElementById('mypageLink');
-    const mypageMenu = document.getElementById('mypageMenu');
-
-    mypageLink.addEventListener('mouseover', function () {
-        mypageMenu.style.display = 'block';
-    });
-
-    mypageLink.addEventListener('mouseout', function () {
-        mypageMenu.style.display = 'none';
-    });
 });
+
+
